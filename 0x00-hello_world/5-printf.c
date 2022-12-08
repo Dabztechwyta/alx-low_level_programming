ecbf97241b1a81eea9d5ca5cbdecaa2cea374caa
Dabztechwyta <healervic17@gmail.com>
2022-12-08 03:03:00 -0800

    C hello-world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the str with proper grammar,
+ * but the outcome is a piece of art, the new line
+ * Return: 0 when exited
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
